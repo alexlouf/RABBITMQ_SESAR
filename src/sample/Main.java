191cdfc3cf346d97d7887db2b3ad11be8bfc3c8d
Alexandre Louf <contact@alexandrelouf.fr>
2021-05-26 16:04:17 +0200

    initial commit

---
+++ src/sample/Main.java
@@ -0,0 +1,47 @@
+package sample;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+import com.rabbitmq.client.Channel;
+import com.rabbitmq.client.Connection;
+import com.rabbitmq.client.ConnectionFactory;
+import com.rabbitmq.client.DeliverCallback;
+import org.slf4j.*;
+
+public class Main extends Application {
+
+    @Override
+    public void start(Stage primaryStage) throws Exception{
+        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
+        primaryStage.setTitle("Hello World");
+        primaryStage.setScene(new Scene(root, 300, 275));
+        primaryStage.show();
+
+        String QUEUE_NAME = "v1.airbus.woc.atcvolumes";
+        ConnectionFactory factory = new ConnectionFactory();
+        factory.useSslProtocol();
+        factory.setHost("api.dev.pansa.pl");
+        factory.setPort(5671);
+        factory.setVirtualHost("sesar-sol40");
+        factory.setPassword("ACCESS_TOKEN");
+        Connection connection = factory.newConnection();
+        Channel channel = connection.createChannel();
+
+        channel.queueDeclare(QUEUE_NAME, false, false, false, null);
+        System.out.println(" [*] Waiting for messages. To exit press CTRL+C");
+
+        DeliverCallback deliverCallback = (consumerTag, delivery) -> {
+            String message = new String(delivery.getBody(), "UTF-8");
+            System.out.println(" [x] Received '" + message + "'");
+        };
+        channel.basicConsume(QUEUE_NAME, true, deliverCallback, consumerTag -> { });
+    }
+
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
